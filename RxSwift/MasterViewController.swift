b21069d091ffb1aabac604b75e996cab6e6c167d
Asep Zee <asep.abdulazis5@gmail.com>
2020-01-29 22:24:26 +0700

    add RxSwift dependency

--- RxSwift/MasterViewController.swift
+++ RxSwift/MasterViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class MasterViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func selectCharacter(_ sender: UIBarButtonItem) {
+        let detailVC = storyboard?.instantiateViewController(identifier: "DetailViewController") as! DetailViewController
+        detailVC.delegate = self
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+}
 
-
+extension MasterViewController: CharacterDelegate{
+    func didSelectCharacter(_ name: String) {
+        titleLabel.text = name
+    }
+    
+    
 }
 
