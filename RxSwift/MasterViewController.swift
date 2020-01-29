62d9c7685029d7e4f6bd66fc981fbfc26faac45a
Asep Zee <asep.abdulazis5@gmail.com>
2020-01-29 22:53:08 +0700

    add rxswift masterViewcontroller

--- RxSwift/MasterViewController.swift
+++ RxSwift/MasterViewController.swift
@@ -7,17 +7,25 @@
 //
 
 import UIKit
+import RxSwift
 
 class MasterViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
+    let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func selectCharacter(_ sender: UIBarButtonItem) {
-        
+        let detailVC = storyboard?.instantiateViewController(identifier: "DetailViewController") as! DetailViewController
+        //        detailVC.delegate = self
+                detailVC.selectedCharacter
+                    .subscribe (onNext: { [weak self] character in
+                        self?.titleLabel.text = "\(character)"
+                    }).disposed(by: disposeBag)
+                navigationController?.pushViewController(detailVC, animated: true)
     }
     
 }
